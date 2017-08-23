0d0f2fa1bc8ea7e209f36f405a0093016b54c304
PRYSM\JBlackford <jblackford@prysm.com>
2017-08-23 15:17:15 -0400

    Deleting old repo, replacing with updated version from new template generator

--- Synthesis.GuestService.Modules/Dao/IRepositoryFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Synthesis.GuestService.Dao
-{
-    public interface IRepositoryFactory
-    {
-        IBaseRepository<T> CreateRepository<T>() where T : class;
-    }
-}
--- Synthesis.GuestService.Modules/Responses/BaseResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace Synthesis.GuestService.Modules.Responses
-{
-    public class BaseResponse
-    {
-        public int StatusCode { get; set; }
-    }
-}
