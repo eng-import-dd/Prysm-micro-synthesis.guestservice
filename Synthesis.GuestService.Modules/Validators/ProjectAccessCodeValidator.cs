69d3abeb50248ecc7aef64eb7ce577e3219066da
Jason Blackford <JBlackford@prysm.com>
2017-11-02 11:39:31 -0400

    Updated ProjectAccessCode validation message wording

--- Synthesis.GuestService.Modules/Validators/ProjectAccessCodeValidator.cs
+++ Synthesis.GuestService.Modules/Validators/ProjectAccessCodeValidator.cs
@@ -7,7 +7,7 @@ namespace Synthesis.GuestService.Validators
         public ProjectAccessCodeValidator() : base("ProjectAccessCode")
         {
             RuleFor(request => request)
-                .Length(10).WithMessage("The ProjectAccessCode field must not be 10 characters in length");
+                .Length(10).WithMessage("The ProjectAccessCode field must be 10 characters in length");
             RuleFor(request => request)
                 .Must(x => int.TryParse(x, out var _))
                 .WithMessage("The access code must be a number");
