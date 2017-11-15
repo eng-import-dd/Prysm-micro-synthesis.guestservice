344e3c619540aa65658ba59ac26d36e3fa060801
Eli Collis <ecollis@prysm.com>
2017-11-15 14:48:58 -0500

    CU-414 - add data contract & data member to GuestInvite & GuestSession

--- Synthesis.Guest.Modules/Models/GuestInvite.cs
+++ Synthesis.Guest.Modules/Models/GuestInvite.cs
@@ -1,23 +1,49 @@
 using System;
+using System.Runtime.Serialization;
+using Microsoft.Hadoop.Avro;
 using Newtonsoft.Json;
 
 namespace Synthesis.GuestService.Models
 {
+    [DataContract]
     public class GuestInvite
     {
-        public DateTime? CreatedDateTime { get; set; }
-        public string FirstName { get; set; }
-        public DateTime? GuestAccessCodeCreatedDateTime { get; set; }
-        public string GuestEmail { get; set; }
-
         [JsonProperty("id")]
         public Guid Id { get; set; }
 
+        [DataMember]
+        public DateTime? CreatedDateTime { get; set; }
+
+        [DataMember]
+        [NullableSchema]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        [NullableSchema]
+        public DateTime? GuestAccessCodeCreatedDateTime { get; set; }
+
+        [DataMember]
+        public string GuestEmail { get; set; }
+
+        [DataMember]
         public Guid InvitedBy { get; set; }
+
+        [DataMember]
+        [NullableSchema]
         public DateTime? LastAccessDate { get; set; }
+
+        [DataMember]
+        [NullableSchema]
         public string LastName { get; set; }
+
+        [DataMember]
+        [NullableSchema]
         public string ProjectAccessCode { get; set; }
+
+        [DataMember]
         public Guid ProjectId { get; set; }
+
+        [DataMember]
         public Guid UserId { get; set; }
     }
 }
