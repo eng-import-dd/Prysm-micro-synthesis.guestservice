30c6b9b2df0ae6aa3c22c8581d4444e26ed36640
PRYSM1762\Jeremy <jshull@prysm.com>
2018-06-13 11:48:21 -0400

    CU-609 Added message to validator

--- Synthesis.Guest.Modules/Validators/GetGuestInvitesRequestValidator.cs
+++ Synthesis.Guest.Modules/Validators/GetGuestInvitesRequestValidator.cs
@@ -8,8 +8,8 @@ namespace Synthesis.GuestService.Validators
     {
         public GetGuestInvitesRequestValidator()
         {
-            RuleFor(request => request.GuestEmail).NotEmpty().When(request => request.GuestUserId == null);
-            RuleFor(request => request.GuestUserId).NotEmpty().When(request => request.GuestEmail == null);
+            RuleFor(request => request.GuestEmail).NotEmpty().When(request => request.GuestUserId == null).WithMessage("An email address must be specified if not UserId is provided");
+            RuleFor(request => request.GuestUserId).NotEmpty().When(request => request.GuestEmail == null).WithMessage("A UserId amust be specified if no email address is provided"); ;
         }
     }
 }
