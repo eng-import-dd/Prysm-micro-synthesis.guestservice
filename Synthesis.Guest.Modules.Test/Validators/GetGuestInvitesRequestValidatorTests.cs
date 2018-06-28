b805962d3e18e60511a6c17a2962d67600b76c59
PRYSM1762\Jeremy <jshull@prysm.com>
2018-06-28 13:07:25 -0400

    CU-609 Added missing validator tests

---
+++ Synthesis.Guest.Modules.Test/Validators/GetGuestInvitesRequestValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using Synthesis.GuestService.InternalApi.Models;
+using Synthesis.GuestService.Validators;
+using Xunit;
+
+namespace Synthesis.GuestService.Modules.Test.Validators
+{
+    public class GetGuestInvitesRequestValidatorTests
+    {
+        private readonly GetGuestInvitesRequestValidator _validator = new GetGuestInvitesRequestValidator();
+        private readonly GetGuestInvitesRequest _defaultRequest = GetGuestInvitesRequest.Example();
+
+        [Fact]
+        public void ShouldPassForValidRequest()
+        {
+            var result = _validator.Validate(_defaultRequest);
+
+            Assert.True(result.IsValid);
+        }
+    }
+}
